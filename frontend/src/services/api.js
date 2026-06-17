afcac248a783b8e6e3030b90e11fabfaa8c1109f
yogesh0525 <eyogeshkumar123@gmail.com>
2026-06-17 11:00:01 +0530

    Updated API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/users",
+  baseURL: "https://atmapplication.onrender.com/api/users",
 });
 
 export const login = (accountNumber, pin) =>
